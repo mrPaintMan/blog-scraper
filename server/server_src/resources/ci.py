c31f06bf850dd1e00f749ea80c5c4af683bc92bf
Filip Palmqvist <fille.palmqvist@icloud.com>
2020-06-22 19:22:03 +0200

    fixed bug with ci's webhook

--- server/server_src/resources/ci.py
+++ server/server_src/resources/ci.py
@@ -8,7 +8,7 @@ class Ci(Resource):
     @staticmethod
     def post():
         url = "http://jenkins:kallekula@192.168.0.100:8080/github-webhook/"
-        response = requests.post(url, headers=request.headers, data=request.json)
+        response = requests.post(url, headers=request.headers, json=request.json)
 
         return {"status": "{}".format(response.content)}, response.status_code
 
