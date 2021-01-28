f44914c8ec0f2e83f3e4aa5641dd4d3018cec0f6
Filip Palmqvist <fille.palmqvist@icloud.com>
2021-01-28 15:28:57 +0100

    Handled Grounded's new cookie consent

--- selenium-scrapers/selenium_src/scrapers/grounded_scraper.py
+++ selenium-scrapers/selenium_src/scrapers/grounded_scraper.py
@@ -38,9 +38,16 @@ def scrape():
     data = []
 
     xpath_to_pag = "/html/body/div/main/div/section[2]/div/div[1]/div[2][@class='pagination']/button"
+    xpath_to_cookie_consent = "/html/body/div[2]/div/button"
 
     # Wait for page to load
     WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, xpath_to_pag)))
+
+    cookie_consent = driver.find_element_by_xpath(xpath_to_cookie_consent)
+
+    if cookie_consent is not None:
+        cookie_consent.click()
+        WebDriverWait(driver, 5).until_not(ec.invisibility_of_element((By.XPATH, xpath_to_cookie_consent)))
 
     # Click the "load more" button until it disappears
     while True:
