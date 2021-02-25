f55a4b729c7ed3239ac6ead769cd4a23d3d46235
Filip Palmqvist <fille.palmqvist@icloud.com>
2021-02-25 22:11:07 +0100

    added dynamic update to 2nd extinction alt_image

--- common-scrapers/common_src/scrapers/second_extinction_scraper.py
+++ common-scrapers/common_src/scrapers/second_extinction_scraper.py
@@ -3,8 +3,9 @@ from common_src.lib.model.source import Source
 from common_src.scrapers.abstract_scraper import make_soup, remove_dups, now
 
 SOURCE_CODE = "second_extinction"
-WEBSITE = "https://www.secondextinctiongame.com/news"
-ALT_IMAGE = 'https://www.secondextinctiongame.com/static/242486b363d867dc483deb6d7038dde1/d8255/se_screenshot_5.jpg'
+BASESITE = "https://www.secondextinctiongame.com"
+WEBSITE = BASESITE + "/news"
+MEDIASITE = BASESITE + "/media"
 FILENAME = "../resources/data/second_extinction.txt"
 
 
@@ -12,21 +13,28 @@ def get_source():
     name = "Second Extinction"
     description = 'Second Extinction is a first person shooter game where earth has been invaded by mutated dinosaurs.'
     profile_image = 'https://www.secondextinctiongame.com/static/logo-0d52f8575a251eff8ebd6e2d6bd6c51b.png'
-    return Source(SOURCE_CODE, name, description, profile_image, ALT_IMAGE, None)
+    return Source(SOURCE_CODE, name, description, profile_image, get_alt_image(), None)
+
+
+def get_alt_image():
+    soup = make_soup(MEDIASITE)
+    print(BASESITE + soup.findAll("ul")[3].findAll("a", {"title": "Open large image"})[-1].get("href"))
+    return BASESITE + soup.findAll("ul")[3].findAll("a", {"title": "Open large image"})[-1].get("href")
 
 
 def scrape():
+    alt_image = get_alt_image()
     soup = make_soup(WEBSITE)
-    base_site = "https://www.secondextinctiongame.com"
+
     data = []
 
     for post in soup.findAll("section")[1].findAll("article"):
 
         date = post.find("time").text.replace("-", "") + "0000"
         title = post.find("h3").text.strip()
-        link = base_site + post.find("a").get("href")
-        alt_image = ALT_IMAGE
-        image = base_site + post.find("picture").find("img").get("src").replace(" ", "%20")
+        link = BASESITE + post.find("a").get("href")
+        alt_image = alt_image
+        image = BASESITE + post.find("picture").find("img").get("src").replace(" ", "%20")
 
         data.append(Post(None, date, title, link, image, alt_image, SOURCE_CODE, None))
 
